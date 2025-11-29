5cffc91ae8f5558c76d3d7cab2f04012cfa40e0b
olimjonof <olimjonovmuhammad007@gmail.com>
2025-11-29 14:36:28 +0200

    online-book-shop
    online-book-shop

---
+++ Portfolio/js/script.js
@@ -0,0 +1,7 @@
+/*  ==============typing animation================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Java Backend Developer","Python Backend Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
